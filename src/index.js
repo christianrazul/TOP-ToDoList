5ad384cab0fa53168b44845ece70ea95dc6ca0d5
christianrazul <razulchristian@gmail.com>
2023-11-28 23:57:31 +0800

    update functionality for adding a task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const addTaskForm = document.querySelector("[data-add-task-form]");
 const taskList = document.querySelector("[data-task-list]");
 
-addTaskForm.addEventListener("click", (element) => {
+addTaskForm.addEventListener("submit", (element) => {
   element.preventDefault();
+  addTaskItem();
 });
+
+function addTaskItem() {
+  const taskItemTitle = document.querySelector("[data-task-title]");
+  const taskItem = document.createElement("li");
+
+  taskItem.innerHTML = `
+  <div class="unchecked"></div>
+  <div class="list-info">
+    <h3 class="list-info-title">${taskItemTitle.value}</h3>
+    <h4 class="list-info-src">Test</h4>
+  </div>
+  `;
+
+  taskList.appendChild(taskItem);
+}
